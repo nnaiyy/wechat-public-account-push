5d5310a98b00ff95fb893c04558eb1a4ed431193
nnaiyy <111975125+nnaiyy@users.noreply.github.com>
2022-10-11 17:05:02 +0800

    Update index.cjs

--- 配置/index.cjs
+++ 配置/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oTxKO6VYQi29PtE1r5EnFnlq5Bjs',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '8FXyFfUEx1CAOeJ3GP8wLdRRomo7FICzE5k9PsCRc_o	',
+      useTemplateId: 'B7UpUpRBzzgjxknHEDlt-uRo0eNPKeTwfOOjd3fp1tA	',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
